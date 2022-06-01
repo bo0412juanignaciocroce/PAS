45a22c3576511ec5ee2ebf31d45d2228ed25934b
bo0412juanignaciocroce <juanignacio.croce@alumnos.upm.es>
2022-06-01 18:52:33 +0200

    Added local storage through room. Tested and working.

--- app/src/main/java/es/upm/etsisi/pas/MainActivity.java
+++ app/src/main/java/es/upm/etsisi/pas/MainActivity.java
@@ -1,8 +1,11 @@
 package es.upm.etsisi.pas;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +17,8 @@ import es.upm.etsisi.pas.json.PeliculasPojo;
 import es.upm.etsisi.pas.recicler_view_adapters.PeliculasPojoResultAdapter;
 import es.upm.etsisi.pas.json.Result;
 import es.upm.etsisi.pas.json.TheMovieDatabaseService;
+import es.upm.etsisi.pas.sqlite_local_database.UsuariosEntity;
+import es.upm.etsisi.pas.sqlite_local_database.UsuariosRepository;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Retrofit;
@@ -49,5 +54,25 @@ public class MainActivity extends AppCompatActivity {
                 .flatMapIterable(PeliculasPojo::getResults)
                 .map( x -> datos.add(x) )
                 .subscribe( x -> adapter.notifyDataSetChanged() );
+
+        UsuariosRepository ur = new UsuariosRepository(this.getApplication());
+        ur.deleteAll(); /* //TODO PLEASE REMOVE THIS LINE BEFORE THE END */
+        final Observer<List<UsuariosEntity>> ueo =
+                new Observer<List<UsuariosEntity>>() {
+                    @Override
+                    public void onChanged(List<UsuariosEntity> listLiveData) {
+                        for(UsuariosEntity usuariosEntity : listLiveData) {
+                            Log.e("Warning", usuariosEntity.getNombre() + " : " +
+                                    usuariosEntity.getPassword() + " : " +
+                                    Float.toString(usuariosEntity.getRol()));
+                        }
+                    }
+                };
+        ur.getAll().observe(this,ueo);
+
+        ur.insert(new UsuariosEntity("Usr1","Pwd1",(float)1.0));
+        ur.insert(new UsuariosEntity("Usr2","Pwd2",(float)2.0));
+        ur.insert(new UsuariosEntity("Usr3","Pwd3",(float)3.0));
+        ur.insert(new UsuariosEntity("Usr4","Pwd4",(float)4.0));
     }
 }
