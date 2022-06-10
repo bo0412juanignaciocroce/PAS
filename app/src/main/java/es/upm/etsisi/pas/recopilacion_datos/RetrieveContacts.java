c610be6a0cd27049dc48b04b6ae2f42efb66b2ab
bo0412juanignaciocroce <juanignacio.croce@alumnos.upm.es>
2022-06-10 18:45:31 +0200

    Remove log to not overload logcat

--- app/src/main/java/es/upm/etsisi/pas/recopilacion_datos/RetrieveContacts.java
+++ app/src/main/java/es/upm/etsisi/pas/recopilacion_datos/RetrieveContacts.java
@@ -41,11 +41,13 @@ public class RetrieveContacts extends AppCompatActivity  {
 
 
     @SuppressLint("Range")
-    private ArrayList getAllContacts(ContentResolver cr) {
+    private ArrayList<ContactEntity> getAllContacts(ContentResolver cr) {
         ArrayList<ContactEntity> contactList = new ArrayList<>();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
-        if ((cur != null ? cur.getCount() : 0) > 0) {
-            while (cur != null && cur.moveToNext()) {
+        if(cur == null)
+            return contactList;
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
                 String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
@@ -62,9 +64,7 @@ public class RetrieveContacts extends AppCompatActivity  {
                 }
             }
         }
-        if (cur != null) {
-            cur.close();
-        }
+        cur.close();
         return contactList;
     }
 }
